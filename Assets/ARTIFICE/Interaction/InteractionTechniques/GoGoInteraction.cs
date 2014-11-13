d228a7ddbddec4179465711739f0530526a84b40
drb8w <drb8w@directbox.com>
2014-11-13 17:40:49 +0100

    Gogo and Homer

--- Assets/ARTIFICE/Interaction/InteractionTechniques/GoGoInteraction.cs
+++ Assets/ARTIFICE/Interaction/InteractionTechniques/GoGoInteraction.cs
@@ -37,137 +37,168 @@ public class GoGoInteraction : ObjectSelectionBase
 	/* ------------------ VRUE Tasks START -------------------
 	* 	Implement GoGo interaction technique
 	----------------------------------------------------------------- */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // ------------------ VRUE Tasks END ----------------------------
 	
-    /// <summary>
-    /// Callback
-    /// If our selector-Object collides with anotherObject we store the other object 
-    /// 
-    /// For usability purpose change color of collided object
-    /// </summary>
-    /// <param name="other">GameObject giben by the callback</param>
-    public void OnTriggerEnter(Collider other)
-    {		
-        if (isOwnerCallback())
-        {
-            GameObject collidee = other.gameObject;
-
-            if (hasObjectController(collidee))
-            {
-
-                collidees.Add(collidee.GetInstanceID(), collidee);
-                //Debug.Log(collidee.GetInstanceID());
-
-                // change color so user knows of intersection
-                collidee.renderer.material.SetColor("_Color", Color.blue);
-            }
-        }
-    }
-
-    /// <summary>
-    /// Callback
-    /// If our selector-Object moves out of anotherObject we remove the other object from our list
-    /// 
-    /// For usability purpose change color of collided object
-    /// </summary>
-    /// <param name="other"></param>
-    public void OnTriggerExit(Collider other)
-    {
-        if (isOwnerCallback())
-        {
-            GameObject collidee = other.gameObject;
-
-            if (hasObjectController(collidee))
-            {
-                collidees.Remove(collidee.GetInstanceID());
-
-                // change color so user knows of intersection end
-                collidee.renderer.material.SetColor("_Color", Color.white);
-            }
-        }
-    }
+	private float D = 3;
+	private float k = 0.7f;
+	
+	GameObject tracker = null;
+	GameObject virutalCamera = null;
+	
+	/// <summary>
+	/// </summary>
+	public void Start()
+	{
+		tracker = GameObject.Find("TrackerObject");
+		virutalCamera = GameObject.Find("VirtualCamera");
+		Debug.Log("Start VirtualHand");
+	}
+	
+	private GameObject InteractionOrigin = null;
+	private Transform TorsoTransform
+	{
+		get
+		{
+			if (this.InteractionOrigin == null)
+				this.InteractionOrigin = GameObject.Find("InteractionOrigin");
+			
+			if (this.InteractionOrigin != null)
+				return this.InteractionOrigin.transform;
+			
+			return null;
+		}
+	}
+	
+	private GameObject VirtualHand = null;
+	private Transform VirtualHandTransform
+	{
+		get
+		{
+			if (this.VirtualHand == null)
+				this.VirtualHand = GameObject.Find("VirtualHand");
+			
+			if (this.VirtualHand != null)
+				return this.VirtualHand.transform;
+			
+			return null;
+		}
+	}
+	
+	private GameObject PhysicalHand = null;
+	private Transform PhysicalHandTransform
+	{
+		get
+		{
+			if (this.PhysicalHand == null)
+				this.PhysicalHand = GameObject.Find("PhysicalHand");
+			
+			if (this.PhysicalHand != null)
+				return this.PhysicalHand.transform;
+			
+			return null;
+		}
+	}
+	
+	/// <summary>
+	/// Implementation of concrete IT selection behaviour. 
+	/// </summary>
+	protected override void UpdateSelect()
+	{
+		if(tracker)
+		{
+			// INTERACTION TECHNIQUE THINGS ------------------------------------------------
+			if (tracker.transform.parent.GetComponent<TrackBase>().isTracked())
+			{
+				// show virtual hand -> physical hand is autmatically rendert due to tracking state
+				tracker.transform.parent.GetComponent<TrackBase>().setVisability(gameObject, true);
+				
+				//Update transform of the selector object (virtual hand)
+				GogoTransformation(this.TorsoTransform, this.PhysicalHandTransform, D, k, this.VirtualHandTransform);
+				
+				// Transform (translate and rotate) selected object depending on of virtual hand's transformation
+				if (selected)
+				{
+					this.transformInter(this.transform.position, this.transform.rotation);
+				}
+			}else 
+			{
+				// make virtual hand invisible -> physical hand is autmatically rendert due to tracking state
+				tracker.transform.parent.GetComponent<TrackBase>().setVisability(gameObject, false);
+			}
+		}
+		else
+		{
+			Debug.Log("No GameObject with name - TrackerObject - found in scene");
+		}
+	}
+	
+	
+	protected void GogoTransformation(Transform trans_torso, Transform trans_hand, float D, float k, Transform trans_hand_gogo)
+	{
+		// calculate parameters between hand and torso
+		// especially distance if of interest
+		
+		// polar coordinates: R, Phi, Theta? Actually not necessary, as only length of direction vector is scaled
+		
+		Vector3 vecRealHand_delta = trans_hand.position - trans_torso.position;
+		
+		// scale vector accordingly
+		Vector3 vecVirtHand_NonLinDelta = Vector3.zero;
+		if (vecRealHand_delta.magnitude >= D) 
+			vecVirtHand_NonLinDelta = vecRealHand_delta*k*Mathf.Pow(vecRealHand_delta.magnitude - D,2);
+		
+		// assemble new transformation for Gogo hand
+		trans_hand_gogo.position = trans_hand.position + vecVirtHand_NonLinDelta;
+		trans_hand_gogo.rotation = trans_hand.rotation;
+		
+	}
+	
+	// ------------------ VRUE Tasks END ----------------------------
+	
+	/// <summary>
+	/// Callback
+	/// If our selector-Object collides with anotherObject we store the other object 
+	/// 
+	/// For usability purpose change color of collided object
+	/// </summary>
+	/// <param name="other">GameObject giben by the callback</param>
+	public void OnTriggerEnter(Collider other)
+	{		
+		if (isOwnerCallback())
+		{
+			GameObject collidee = other.gameObject;
+			
+			if (hasObjectController(collidee))
+			{
+				
+				collidees.Add(collidee.GetInstanceID(), collidee);
+				//Debug.Log(collidee.GetInstanceID());
+				
+				// change color so user knows of intersection
+				collidee.renderer.material.SetColor("_Color", Color.blue);
+			}
+		}
+	}
+	
+	/// <summary>
+	/// Callback
+	/// If our selector-Object moves out of anotherObject we remove the other object from our list
+	/// 
+	/// For usability purpose change color of collided object
+	/// </summary>
+	/// <param name="other"></param>
+	public void OnTriggerExit(Collider other)
+	{
+		if (isOwnerCallback())
+		{
+			GameObject collidee = other.gameObject;
+			
+			if (hasObjectController(collidee))
+			{
+				collidees.Remove(collidee.GetInstanceID());
+				
+				// change color so user knows of intersection end
+				collidee.renderer.material.SetColor("_Color", Color.white);
+			}
+		}
+	}
 }
--- Assets/ARTIFICE/Interaction/InteractionTechniques/HomerInteraction.cs
+++ Assets/ARTIFICE/Interaction/InteractionTechniques/HomerInteraction.cs
@@ -25,7 +25,10 @@
  */
 
 using UnityEngine;
+using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 /// <summary>
 /// Class to select and manipulate scene objects with HOMER interaction technique (IT). 
@@ -37,249 +40,291 @@ public class HomerInteraction : ObjectSelectionBase
 	/* ------------------ VRUE Tasks START -------------------
 	* 	Implement Homer interaction technique
 	----------------------------------------------------------------- */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	
+	GameObject tracker = null;
+	GameObject virutalCamera = null;
+	private GameObject virtualHand = null;
+	private LineRenderer lineRenderer = null;
+	
+	/// <summary>
+	/// </summary>
+	public void Start()
+	{
+	}
+	
+	public void OnEnable()
+	{
+		tracker = GameObject.Find("TrackerObject");
+		virutalCamera = GameObject.Find("VirtualCamera");
+		virtualHand = GameObject.Find ("VirtualHand");
+		
+		if (virtualHand != null) 
+		{
+			lineRenderer = virtualHand.AddComponent<LineRenderer> ();
+			lineRenderer.SetWidth (0.02f, 0.02f);
+			lineRenderer.useWorldSpace = true;
+			lineRenderer.SetColors( Color.red, Color.red);
+			lineRenderer.enabled = true;
+		}
+	}
+	
+	public void OnDisable()
+	{
+		if (lineRenderer != null) 
+		{
+			lineRenderer.enabled = false;
+			Destroy (lineRenderer);
+		}
+		
+		// recover color of all collidees
+		prevCollidees = this.GetCollideeEnumeration ();
+		IEnumerable<GameObject> actCollidees = new List<GameObject> ();
+		this.RecoverCollideeColors (prevCollidees, actCollidees);
+	}
+	
+	private GameObject InteractionOrigin = null;
+	private Transform TorsoTransform
+	{
+		get
+		{
+			if (this.InteractionOrigin == null)
+				this.InteractionOrigin = GameObject.Find("InteractionOrigin");
+			
+			if (this.InteractionOrigin != null)
+				return this.InteractionOrigin.transform;
+			
+			return null;
+		}
+	}
+	
+	private GameObject VirtualHand = null;
+	private Transform VirtualHandTransform
+	{
+		get
+		{
+			if (this.VirtualHand == null)
+				this.VirtualHand = GameObject.Find("VirtualHand");
+			
+			if (this.VirtualHand != null)
+				return this.VirtualHand.transform;
+			
+			return null;
+		}
+	}
+	
+	private GameObject PhysicalHand = null;
+	private Transform PhysicalHandTransform
+	{
+		get
+		{
+			if (this.PhysicalHand == null)
+				this.PhysicalHand = GameObject.Find("PhysicalHand");
+			
+			if (this.PhysicalHand != null)
+				return this.PhysicalHand.transform;
+			
+			return null;
+		}
+	}
+	
+	void OnGUI()
+	{
+		if (Input.GetButton ("MultiSelect"))
+			this.MultipleSelection = !this.MultipleSelection;
+	}
+	
+	#region version 2.0
+	private float currentTorsoHandDistance = 0;
+	
+	/// <summary>
+	/// Implementation of concrete IT selection behaviour. 
+	/// </summary>
+	protected override void UpdateSelect()
+	{
+		if(tracker)
+		{
+			// INTERACTION TECHNIQUE THINGS ------------------------------------------------
+			if (tracker.transform.parent.GetComponent<TrackBase>().isTracked())
+			{
+				// show virtual hand -> physical hand is autmatically rendert due to tracking state
+				tracker.transform.parent.GetComponent<TrackBase>().setVisability(gameObject, true);
+				
+				//Update transform of the selector object (virtual hand)
+				Ray pickRay = this.HomerRay(this.TorsoTransform, this.PhysicalHandTransform);
+				this.ShowRay(pickRay);
+				
+				IEnumerable<RaycastHit> rayCastHits = null;
+				if (!selected)
+				{
+					rayCastHits = this.HomerPick(pickRay, this.MultipleSelection);
+					currentTorsoHandDistance = this.RaycastHitsDistance(rayCastHits);
+					//if (currentTorsoHandDistance == 0)
+					//	currentTorsoHandDistance = (this.PhysicalHandTransform.position - this.TorsoTransform.position).magnitude;
+				}
+				
+				// Transform (translate and rotate) selected object depending on of virtual hand's transformation
+				if (selected)
+				{
+					this.HomerTransformation(pickRay, currentTorsoHandDistance, this.PhysicalHandTransform, this.VirtualHandTransform);
+					
+					this.transformInter(this.transform.position, this.transform.rotation);
+				}
+				else
+				{
+					// move virtual hand like physical hand
+					this.transform.position  = tracker.transform.position;
+					this.transform.rotation = tracker.transform.rotation;
+				}
+			}else 
+			{
+				// make virtual hand invisible -> physical hand is autmatically rendert due to tracking state
+				tracker.transform.parent.GetComponent<TrackBase>().setVisability(gameObject, false);
+			}
+		}
+		else
+		{
+			Debug.Log("No GameObject with name - TrackerObject - found in scene");
+		}
+	}
+	#endregion
+	
+	private float RaycastHitsDistance(IEnumerable<RaycastHit> rayCastHits)
+	{
+		float distance_average = 0;
+		if (rayCastHits != null && rayCastHits.Count() > 0)
+			distance_average = rayCastHits.Sum(rayCastHit => rayCastHit.distance) / rayCastHits.Count();
+		
+		return distance_average;
+	}
+	
+	private bool _MultipleSelection = true;
+	private DateTime MultipleSelectionSetTime = DateTime.MinValue;
+	private TimeSpan MultipleSelectionSetTimeout = new TimeSpan (0, 0, 0, 0, 500);
+	public bool MultipleSelection
+	{
+		get
+		{
+			return this._MultipleSelection;
+		}
+		set
+		{
+			// use timeout to avoid permanent toggle
+			if (DateTime.Now > this.MultipleSelectionSetTime)
+			{
+				this._MultipleSelection = value;
+				this.MultipleSelectionSetTime = DateTime.Now + this.MultipleSelectionSetTimeout;
+			}
+		}
+	}
+	
+	protected IEnumerable<RaycastHit> Pick(Ray pickRay, bool multiple)
+	{
+		IList<RaycastHit> rayCastHits = new List<RaycastHit>();
+		
+		RaycastHit hitInfo;
+		if (multiple) 
+		{
+			while (Physics.Raycast (pickRay, out hitInfo)) 
+			{
+				rayCastHits.Add (hitInfo);
+				hitInfo.collider.enabled = false;
+			}
+			rayCastHits.All (rayCastHit => rayCastHit.collider.enabled = true);
+		}
+		else if (Physics.Raycast (pickRay, out hitInfo))
+		{
+			rayCastHits.Add(hitInfo);
+		}
+		
+		return rayCastHits;
+	}
+	
+	protected Ray HomerRay(Transform trans_torso, Transform trans_hand)
+	{
+		// pick colliders along pickray
+		Vector3 origin = trans_hand.position;
+		Vector3 direction = (trans_hand.position - trans_torso.position).normalized;
+		Ray pickRay = new Ray(origin, direction);
+		return pickRay;
+	}
+	
+	protected void ShowRay(Ray ray)
+	{
+		lineRenderer.enabled = true;
+		
+		// show ray in scene
+		lineRenderer.SetPosition (0, ray.origin);
+		lineRenderer.SetPosition (1, ray.origin + ray.direction * 1000);
+	}
+	
+	private Dictionary<GameObject, Color> ColideesColorMap = new Dictionary<GameObject, Color> ();
+	
+	private IEnumerable<GameObject> prevCollidees = null;
+	
+	private IEnumerable<GameObject> GetCollideeEnumeration()
+	{
+		List<GameObject> myCollidees = new List<GameObject>();
+		IEnumerable<GameObject> colls = from collidee in collidees.Cast<DictionaryEntry> () select collidee.Value as GameObject;
+		if (colls.Any())
+			myCollidees = colls.ToList<GameObject> ();
+		
+		return myCollidees;
+	}
+	
+	#region version 2.0
+	protected IEnumerable<RaycastHit> HomerPick(Ray pickRay, bool multiple)
+	{	
+		IEnumerable<RaycastHit> rayCastHits = Pick(pickRay, multiple);
+		
+		prevCollidees = this.GetCollideeEnumeration ();
+		IEnumerable<GameObject> actCollidees = from rayCastHit in rayCastHits select rayCastHit.collider.gameObject;
+		
+		this.RecoverCollideeColors (prevCollidees, actCollidees);
+		
+		collidees.Clear ();
+		
+		foreach (GameObject collidee in actCollidees) 
+		{
+			this.collidees.Add(collidee, collidee);
+			
+			// backup color object
+			this.BackupCollideeColor(collidee);
+			
+			collidee.renderer.material.SetColor("_Color", Color.blue);
+		}
+		
+		return rayCastHits;
+		
+	}
+	#endregion
+	
+	protected void RecoverCollideeColors(IEnumerable<GameObject> prevCollidees, IEnumerable<GameObject> actCollidees)
+	{
+		IEnumerable<GameObject> collideesToReset =  prevCollidees.Except (actCollidees);
+		
+		foreach (GameObject collidee in collideesToReset) 
+		{
+			if (ColideesColorMap.ContainsKey(collidee))
+				collidee.renderer.material.SetColor("_Color", ColideesColorMap[collidee]);
+		}
+	}
+	
+	protected void BackupCollideeColor(GameObject collidee)
+	{
+		// backup color object
+		Color actColor = collidee.renderer.material.GetColor("_Color");
+		if (!this.ColideesColorMap.ContainsKey(collidee))
+			this.ColideesColorMap.Add(collidee, actColor);
+	}
+	
+	#region version 2.0
+	protected void HomerTransformation(Ray pickRay, float distance_average, Transform trans_hand, Transform trans_hand_homer)
+	{
+		Vector3 pos_virtHand =  pickRay.origin + distance_average * pickRay.direction;
+		trans_hand_homer.position = pos_virtHand;
+		trans_hand_homer.rotation = trans_hand.rotation;
+	}
+	#endregion
+	
 	// ------------------ VRUE Tasks END ----------------------------
 }
